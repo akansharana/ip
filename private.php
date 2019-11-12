99118de51f041fd07840d54ec9a94bc8d9899245
Akansha rana <44633873+akansharana@users.noreply.github.com>
2019-11-12 19:25:02 +0530

    Update private.php

--- private.php
+++ private.php
@@ -1,18 +1,13 @@
+ipv4 is a private ip for system
 <?php
-function getAddrByHost($hosts, $timeout = 3) {
-  $returnString = '';
-  foreach ($hosts as $host) {
-    $query = `nslookup -timeout=$timeout -retry=1 $host`;
-    if (preg_match('/\nAddress: (.*)\n/', $query, $matches))
-      $returnString .= trim($matches[1]) . '<br>';
-    $returnString .= $host . '<br>';
-  }
-  return $returnString;
-
+error_reporting(0);
+$hosts = gethostbynamel($hostname);
+if (is_array($hosts)) {
+     echo "Host ".$hostname." resolves to:<br><br>";
+     foreach ($hosts as $ip) {
+          echo "IP: ".$ip."<br>";
+     }
+} else {
+     echo "Host ".$hostname." is not tied to any IP.";
 }
-
-$hostArray[] = 'https://www.w3resource.com';
-
-$returnString = getAddrByHost($hostArray);
-echo $returnString;
 ?>
