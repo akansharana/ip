b40c7af85d6adae1bc3a06b8c60b7f82fbc51042
Akansha rana <44633873+akansharana@users.noreply.github.com>
2019-11-12 19:23:59 +0530

    Update public.php

--- public.php
+++ public.php
@@ -1,3 +1,31 @@
+get client ip useing php
 <?php
-echo getHostByName(php_uname('n'));
+$_SERVER['REMOTE_ADDR']; 
+$ip_address=file_get_contents('http://checkip.dyndns.com/');
+
+$ip_address = str_replace("Current IP Address: ","",$ip_address);
+echo $ip_address ;
 ?>
+
+get client ip using javascript
+<html> 
+
+<head> 
+	
+	
+	<script src= "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"> 
+	</script> 
+
+	<script> 
+$.getJSON("https://api.ipify.org?format=json", 
+function(data) { 
+$("#gfg").html(data.ip); 
+		}) 
+	</script> 
+</head> 
+
+<body> 	
+<p id="gfg"></p> 	
+</body> 
+
+</html> 
